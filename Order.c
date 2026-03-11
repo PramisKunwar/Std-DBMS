5926d9b2a7fb40ddf595a0328f243384fae8ec16
PramisKunwar <pramiskwr123@gmail.com>
2026-03-11 17:10:50 +0545

    Written code to arrange numbers in ascending and descending order

---
+++ Order.c
@@ -0,0 +1,57 @@
+// Sort n numbers in ascending and descending order.       
+#include<stdio.h>
+int main()
+{
+    int a[10],i,j,temp,n;
+    printf("How many numbers? ");
+    scanf("%d",&n);
+
+    for(i=0;i<n;i++)
+    {
+        scanf("%d",&a[i]);
+    }
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]>a[j])
+        {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        }
+    }
+
+    printf("\nAscending is ");
+
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]); 
+    }
+
+    for(i=0;i<n;i++)
+    {
+        for(j=i+1;j<n;j++)
+        {
+            if(a[i]<a[j])
+        {
+            temp = a[i];
+            a[i] = a[j];
+            a[j] = temp;
+        }
+        }
+    }
+
+    printf("\nDescending is ");
+    for(i=0;i<n;i++)
+    {
+        printf("%d\t",a[i]); 
+    }
+
+    return 0;
+}
+
+
+//The core logic behind the ascending and descending is that we can use
