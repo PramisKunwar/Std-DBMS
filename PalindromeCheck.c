8c9ab0f3eb0eb09fd809e25f37c74d54147a66c6
PramisKunwar <pramiskwr123@gmail.com>
2026-03-11 19:15:39 +0545

    Written a program which checks whether a string is palindrome or not

---
+++ PalindromeCheck.c
@@ -0,0 +1,34 @@
+// Write a program to check whether the given string is palindrome or not.
+
+#include<stdio.h>
+#include<string.h>
+int main()
+{
+    char s[10],r[10];
+    int l,i,j=0;
+    printf("enter a string ");
+    scanf("%s",s);
+    l = strlen(s);
+
+    for(i=l;i>=0;i--)
+    {
+        r[j]=s[i];
+        j=j+1;
+    }
+
+    printf("Reverse is ");
+    for(j=0;j<=l;j++)
+    {
+        printf("%c",r[j]);
+    }
+
+    if (strcmp(s,r) == 0)
+    {
+        printf("\n The string is palindrome");
+    }
+    else
+    {
+        printf("\n The string is not palindrome");
+    }
+    return 0;
+}
