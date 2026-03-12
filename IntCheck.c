b75aa79281c26e0d2a9db95bcbc62f88a4d6262f
PramisKunwar <pramiskwr123@gmail.com>
2026-03-12 08:00:22 +0545

    Written IntCheck.c which checks whether a num is + - or 0

---
+++ IntCheck.c
@@ -0,0 +1,29 @@
+// WAP to input number and check whether it is positive ,negative or zero using user defined function
+
+#include<stdio.h>
+void check(int n);
+int main ()
+{
+    int n;
+    printf("Enter a number ");
+    scanf("%d",&n);
+    check(n);
+    return 0;
+}
+
+void check( int n)
+{
+    if(n>0)
+    {
+        printf("\n %d is positive",n);
+    }
+    else if(n<0)
+    {
+        printf("\n %d is negative",n);
+    }
+    else
+    {
+        printf("%d is neutral",n);
+    }
+    
+}
