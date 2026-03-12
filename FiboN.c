2863252b4105792b0a78f52615c20dab553b055e
PramisKunwar <pramiskwr123@gmail.com>
2026-03-12 13:07:36 +0545

    Written FiboN.c which generate Nth term of fibonacci series using recursive function

---
+++ FiboN.c
@@ -0,0 +1,31 @@
+// Program to generate Nth term of fibonacci series using recursive function.
+
+#include<stdio.h>
+int fibo(int n);
+int main()
+{
+    int n,i;
+    printf("Enter a number ");
+    scanf("%d",&n);
+    printf("Fibonacci series is \t ");
+    for(i=1;i<=n;i++)
+    {
+        printf("%d, ",fibo(i));
+    }
+}
+
+int fibo(int n)
+{
+    if(n==0)
+    {
+        return 0;
+    }
+    else if(n==1)
+    {
+        return 1;
+    }
+    else
+    {
+        return (fibo(n-1)+fibo(n-2));
+    }
+}
