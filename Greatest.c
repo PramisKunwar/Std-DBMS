e2fa6e1a70b94c74bd13659c2a48ef312c616426
PramisKunwar <pramiskwr123@gmail.com>
2026-03-25 16:06:13 +0545

    Simplified and cleaned the code

--- Greatest.c
+++ Greatest.c
@@ -1,20 +1,27 @@
-// Program to find the greatest number among 10 number.
+#include <stdio.h>
+#include "utils.h"
 
-#include<stdio.h>
-void findGreatestAmongTen()
-{
-    int i,gre,ar[10];
-    printf("Enter numbers serially \t");
-    for (i=0;i<10;i++)
-    {
-        scanf("%d",&ar[i]);
+void findGreatestAmongTen() {
+    int i, gre, ar[10];
+
+    printf("\nEnter 10 numbers:\n");
+    for(i = 0; i < 10; i++) {
+        printf("  Number %2d: ", i + 1);
+        scanf("%d", &ar[i]);
     }
+
+    printf("\nNumbers: ");
+    for(i = 0; i < 10; i++) { printf("%d", ar[i]); if(i < 9) printf(", "); }
+    printf("\n\nSearching for maximum...\n");
+
     gre = ar[0];
-    for(i = 1; i < 10; i++) 
-    {
-    if(ar[i] > gre) {
-        gre = ar[i];
+    for(i = 1; i < 10; i++) {
+        printf("  Check %2d (%d): ", i+1, ar[i]);
+        if(ar[i] > gre) { printf("New max!\n"); gre = ar[i]; }
+        else             { printf("Not larger\n"); }
+        SLEEP(200);
     }
-    }
-    printf("%d is the greatest number", gre);
+
+    printf("\nResult: %d is the greatest number.\n", gre);
+    printSeparator('=', 40);
 }
--- IntCheck.c
+++ IntCheck.c
@@ -1,28 +1,14 @@
-// Program to check whether input num is positive ,negative or zero using user defined function
+#include <stdio.h>
+#include "utils.h"
 
-#include<stdio.h>
-void check(int n);
-void checkNumberSign()
-{
+void checkNumberSign() {
     int n;
-    printf("Enter a number ");
-    scanf("%d",&n);
-    check(n);
-}
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-void check( int n)
-{
-    if(n>0)
-    {
-        printf("\n %d is positive",n);
-    }
-    else if(n<0)
-    {
-        printf("\n %d is negative",n);
-    }
-    else
-    {
-        printf("%d is neutral",n);
-    }
-    
+    if(n > 0)      printf("\n  %d is POSITIVE (above zero)\n", n);
+    else if(n < 0) printf("\n  %d is NEGATIVE (below zero)\n", n);
+    else           printf("\n  %d is ZERO\n", n);
+
+    printSeparator('=', 40);
 }
