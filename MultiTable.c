8ca84ecdb918be700fb7df2b9568274079912ca5
PramisKunwar <pramiskwr123@gmail.com>
2026-03-25 16:07:17 +0545

    Edited

--- MultiTable.c
+++ MultiTable.c
@@ -1,22 +1,17 @@
-// Program to print multiplication table of an entered number using user define function
+#include <stdio.h>
+#include "utils.h"
 
-#include<stdio.h>
-void mul(int n);
-void printMultiplicationTable()
-{
+void printMultiplicationTable() {
     int n;
-    printf("Enter a number ");
-    scanf("%d",&n);
-    mul(n);
-}
+    printf("Enter a number: ");
+    scanf("%d", &n);
 
-void mul(int n)
-{
-    int i,m;
-    printf("The multiplication table of %d is: \n",n);
-    for(i=1;i<=10;i++)
-    {
-        m = n*i;
-        printf("%d * %d = %d \n",n,i,m);
-    }
+    printf("\nMultiplication Table of %d:\n", n);
+    printf("  +----------+--------+\n");
+    printf("  | Expr     | Result |\n");
+    printf("  +----------+--------+\n");
+    for(int i = 1; i <= 10; i++)
+        printf("  | %2d x %2d  | %6d |\n", n, i, n * i);
+    printf("  +----------+--------+\n");
+    printSeparator('=', 40);
 }
